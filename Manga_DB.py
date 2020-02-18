afa7c6530ba486b1ea8c763ffa9f6e026653a4a5
Maleke <malekelthomas97@gmail.com>
2020-02-18 16:26:39 -0500

    Added comma to (item) in search function of sqliteops.

--- Manga_DB.py
+++ Manga_DB.py
@@ -5,10 +5,13 @@ from sqliteoperations import *
 
 
 def manga_search():
-	books = search_db("mangadb", title_val.get())
+	books = search_db("mangadb.db", title_val.get())
 	if books != None:
 		mangaList.delete("1.0", END)
 		mangaList.insert(END, books)
+	else:
+		mangaList.delete("1.0", END)
+		mangaList.insert(END, "Book not found")
 
 def addManga():
 	pass
@@ -59,13 +62,13 @@ search = Button(window, height = 1, width = 5, text = "Search", command = manga_
 search.grid(row = 2, column = 4)
 
 add_entry = Button(window, height = 1, width = 5, text = "Add") 
-add.grid(row = 3, column = 4)
+add_entry.grid(row = 3, column = 4)
 
 update_entry = Button(window, height = 1, width = 5, text = "Update") 
-update.grid(row = 4, column = 4)
+update_entry.grid(row = 4, column = 4)
 
 delete_entry = Button(window, height = 1, width = 5, text = "Delete") 
-delete.grid(row = 5, column = 4)
+delete_entry.grid(row = 5, column = 4)
 
 close = Button(window, height = 1, width = 5, text = "Close") 
 close.grid(row = 6, column = 4)
--- sqliteoperations.py
+++ sqliteoperations.py
@@ -48,7 +48,7 @@ def update(db_name, quantity, price, item):
 def search_db(db_name, item):
 	conn = sqlite3.connect(db_name)
 	cur = conn.cursor()
-	cur.execute("SELECT * FROM store WHERE item =?", (item))
+	cur.execute("SELECT * FROM store WHERE item =?", (item,))
 	rows = cur.fetchall()
 	conn.close()
 	return rows
