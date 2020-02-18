2b3011ead0177b34a6d4fd0533f44d790c606914
Maleke <malekelthomas97@gmail.com>
2020-02-18 14:48:06 -0500

    Modified sqliteops to take in database name parameter

--- sqliteoperations.py
+++ sqliteoperations.py
@@ -4,15 +4,15 @@ import sqlite3
 # 3 write an sql query
 # 4 commit changes
 # 5 close connection
-def create_table():
-	conn = sqlite3.connect("lite.db ")
+def create_table(db_name):
+	conn = sqlite3.connect(db_name=)
 	cur = conn.cursor()
 	cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
 	conn.commit()
 	conn.close()
 
-def insert(item, quantity, price):
-	conn = sqlite3.connect("lite.db ")
+def insert(db_name, item, quantity, price):
+	conn = sqlite3.connect(db_name)
 	cur = conn.cursor()
 	cur.execute("INSERT INTO store VALUES (?,?,?)",(item, quantity, price))
 	conn.commit()
@@ -23,23 +23,23 @@ create_table()
 insert("Wine Glass", 8, 10)
 #insert("Coffee Cup", 10, 5)
 
-def view():
-	conn = sqlite3.connect("lite.db ")
+def view(db_name):
+	conn = sqlite3.connect(db_name)
 	cur = conn.cursor()
 	cur.execute("SELECT * FROM store")
 	rows = cur.fetchall()
 	conn.close()
 	return rows
 
-def delete(item):
-	conn = sqlite3.connect("lite.db ")
+def delete(db_name, item):
+	conn = sqlite3.connect(db_name)
 	cur = conn.cursor()
 	cur.execute("DELETE FROM store WHERE item =?", (item,))
 	conn.commit()
 	conn.close()
 
-def update(quantity, price, item):
-	conn = sqlite3.connect("lite.db ")
+def update(db_name, quantity, price, item):
+	conn = sqlite3.connect(db_name)
 	cur = conn.cursor()
 	cur.execute("UPDATE store SET quantity=?, price = ? WHERE item =?", (quantity, price, item))
 	conn.commit()
