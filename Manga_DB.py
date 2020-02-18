950774d583cbf9b6dc45d66258e62d9fc2fad6ba
Maleke <malekelthomas97@gmail.com>
2020-02-18 12:38:51 -0500

    Added isbn label and entry. Added search and add buttons

--- Manga_DB.py
+++ Manga_DB.py
@@ -5,17 +5,23 @@ from tkinter import *
 
 window = Tk()
 
-#label for title entry
+#label for title  and ISBN entries
 
 titleLabel = Label(window, height = 1, width = 5, text = "Title") 
 titleLabel.grid(row = 0, column = 0)
 
-#title entry
+isbnLabel = Label(window, height = 1, width = 5, text = "ISBN") 
+isbnLabel.grid(row = 0, column = 2)
+
+#title and ISBN entries
 
 title_val = StringVar()
 title = Entry(window, textvariable = title_val)
 title.grid(row = 0, column = 1)
 
+isbn_val = StringVar()
+isbn = Entry(window, textvariable = title_val)
+isbn.grid(row = 0, column = 3)
 
 #scrollbar for list
 
@@ -31,8 +37,18 @@ mangaList.grid(row = 1, column = 0)
 for i in range(1000):
 	mangaList.insert(END, str(i))
 
-#
+#connect scrollbar to listbox vertically
 sb.config(command=mangaList.yview)
+
+
+#search, add buttons
+
+search = Button(window, height = 1, width = 5, text = "Search") 
+search.grid(row = 1, column = 2)
+
+add = Button(window, height = 1, width = 5, text = "Add") 
+add.grid(row = 2, column = 2)
+
 
 
 
