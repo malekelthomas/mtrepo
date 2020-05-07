ff7959b7bd0d86dbc720ed2c8c236b305d1c5898
Maleke <malekelthomas97@gmail.com>
2020-05-07 07:46:17 -0400

    Added inputToMove func. Updated while loop to add gameMoves. Updated addToBoard to properly change board. Added X/O to player's attributes.

--- TicTac.py
+++ TicTac.py
@@ -12,7 +12,7 @@ class ticTacToeBoard():
 			print("\n-----------")
 	
 	def addToBoard(self, xoro, x, y):
-		self.board[x][y] = xoro
+		self.board[x][y] = "|"+xoro+"|"
     
 	def gameNotFinished(self):
 		if '|_|' in self.board[0]:
@@ -21,10 +21,11 @@ class ticTacToeBoard():
 			return False
 
 class ticTacToePlayer():
-	def __init__(self, name):
+	def __init__(self, name, xoro):
 		self.name = name
 		self.score = 0
-	
+		self.xoro = xoro
+
 	def addToScore(self):
 		self.score+=1
 
@@ -33,22 +34,29 @@ class ticTacToeGame():
 		self.board = ticTacToeBoard()
 		self.players = []
 	
+	def inputToMove(self, move):
+		x = move
+		return move.split(",")
+
 	def startGame(self):
 		player1 = input("Player 1 name?: ")
-		self.players.append(ticTacToePlayer(player1))
+		self.players.append(ticTacToePlayer(player1, "X"))
 
 		player2 = input("Player 2 name?: ")
-		self.players.append(ticTacToePlayer(player2))
+		self.players.append(ticTacToePlayer(player2, "O"))
 
-		
+	
+
 		while self.board.gameNotFinished():
 			print("Players:", self.players[0].name, self.players[1].name,)
 			print("Score: ", self.players[0].score, self.players[1].score)
 			self.board.drawBoard()
-			player1move = input(self.players[0].name+" , where do you want to place X?")
+			player1move = self.inputToMove(input(self.players[0].name+" , where do you want to place X?"))
+			self.board.addToBoard("X", int(player1move[0]), int(player1move[1]))
 			print("..placing")
 			self.board.drawBoard()
-			player2move = input(self.players[1].name+" , where do you want to place O?")
+			player2move = self.inputToMove(input(self.players[1].name+" , where do you want to place O?"))
+			self.board.addToBoard("O", int(player2move[0]), int(player2move[1]))
 			print("..placing")
 			self.board.drawBoard()
 
